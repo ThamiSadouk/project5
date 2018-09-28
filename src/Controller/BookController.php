ed8d0366ffd00492c3ab5437b126f3062720495f
Thami <thami.sadouk@outlook.fr>
2018-09-28 19:19:21 +0200

    created addContact page

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -2,38 +2,53 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\User;
+use App\Repository\UserRepository;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class BookController extends AbstractController
 {
     /**
      * @Route("/", name="home")
+     * la fonction index affiche le profil du user ainsi que la liste de ses contacts
      */
-    public function index()
+    public function index(UserRepository $repo)
     {
+        // cherche la liste des contacts d'un user dans UserRepository
+        $contacts = $repo->findAll(); 
+
         return $this->render('book/index.html.twig', [
             'title' => 'Hello Marsupilami',
+            'contacts' => $contacts
         ]);
     }
 
     /**
-     * @Route("/user/edit", name="edit_profil")
-     */
-    public function editUserProfil() {}
-
-    /**
      * @Route("/contact/add", name="add_contact")
      */
-    public function addContact() {}
+    public function addContact(UserRepository $repo) 
+    {
+        // cherche la liste de tous les contacts dans UserRepository
+        $contacts = $repo->findAll(); 
 
-    /**
-     * @Route("/contact/edit/{id}", name="edit_contact")
-     */
-    public function editContact() {}
+        return $this->render('book/add_contact.html.twig', [
+            'title' => 'Hello Marsupilami',
+            'contacts' => $contacts
+        ]);
+    }
 
     /**
      * @Route("/contact/delete", name="delete_contact")
+     * la fonction delete contact doit supprimer le joint
      */
-    public function deleteContact() {}
+    public function deleteContact() 
+    {}
+
+    /**
+     * @Route("/user/edit", name="edit_profil")
+     */
+    public function editUserProfil() 
+    {}
+
 }
--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
@@ -9,9 +10,18 @@ class UserFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        for($i = 1; $i <= 5; $i++) 
+        {
+            $user = new User(); 
+            $user->setUsername("toto")
+                 ->setEmail("toto@gmail.com")
+                 ->setPassword("test")
+                 ->setDescription("zmlkhqsdf zemlfkh qdfih")
+                 ->setCreatedAt(new \DateTime()); 
+            
+            $manager->persist($user); 
+        }
 
-        $manager->flush();
+        $manager->flush(); 
     }
 }
