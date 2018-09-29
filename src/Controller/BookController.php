c3f02da369b2ebd134a30b358717f99dac868fb4
Thami <thami.sadouk@outlook.fr>
2018-09-29 19:54:57 +0200

    added member_directory view

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -13,40 +13,80 @@ class BookController extends AbstractController
      * @Route("/", name="home")
      * la fonction index affiche le profil du user ainsi que la liste de ses contacts
      */
-    public function index(UserRepository $repo)
+    public function index()
     {
         // On sélection le User logged in ?????
         $user = $this->getUser(); 
 
         // cherche la liste des contacts d'un user dans UserRepository
-        $contacts = $user->getMyContacts(); 
+        $myContacts = $user->getMyContacts(); 
                 
-        //$Mycontacts = $repo->findAll(); 
-
         return $this->render('book/index.html.twig', [
-            'title' => 'Hello Marsupilami',
-            'contacts' => $contacts, 
+            'title' => $user->getUsername(),
+            'contacts' => $myContacts, 
         ]);
     }
 
     /**
-     * @Route("/contact/add", name="add_contact")
+     * @Route("/members", name="member_directory")
      */
-    public function addContact(UserRepository $repo) 
+    public function memberDirectory(UserRepository $repo) 
     {
-        // cherche la liste de tous les contacts dans UserRepository
-        $contacts = $repo->findAll(); 
+        // select all members
+        $members = $repo->findAll(); 
 
-        return $this->render('book/add_contact.html.twig', [
+        return $this->render('book/member_directory.html.twig', [
             'title' => 'Hello Marsupilami',
-            'contacts' => $contacts
+            'members' => $members
         ]);
     }
 
     /**
-     * @Route("/contact/delete", name="delete_contact")
-     * la fonction delete contact doit supprimer la jointure
+     * @Route("/add/contact/{id}", name="add_contact")
      */
-    public function deleteContact() 
-    {}
+    public function addContact($id, UserRepository $repo) 
+    {
+        // On sélectionne le User logged in ?????
+        $user = $this->getUser(); 
+
+        // On sélectionne le contact que l'on souhaite ajouter
+        $contact = $repo->find($id); 
+
+        if (!$contact) 
+        {
+            throw $this->createNotFoundException('Le contact n\'a pas été trouvé'); 
+        }
+
+        // on effectue la relation entre les 2 objects
+        $user->addMyContact($contact); 
+
+        return $this->redirectToRoute('member_directory'); 
+    }
+
+
+    
+    // /**
+    //  * @Route("/contact/remove/{id}", name="delete_contact")
+    //  *  doit supprimer la jointure
+    //  */
+    // public function removeContact($id) 
+    // {
+    //     // on sélectionne notre user
+    //     $user = $this->getUser(); 
+
+    //     // si mes contacts existe alors on peut les supprimer 
+    //     if($user->getMyContacts()) { 
+
+    //         $manager = $this->getDoctrine()->getManager(); 
+
+    //         $contact = $manager->getRepository(User::class)->find($id); 
+    
+    //         if(!$contact) 
+    //         {
+    //             throw $this->createNotFoundException('Le contact n\'a pas pu être supprimé'); 
+    //         }
+    //         $manager->removeMyContact($contact); 
+    //         $manager->flush(); 
+    //     }
+    // }
 }
